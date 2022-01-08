cecb2dcb0f49b5bb18f30f07681682e879ca63b6
Matija Marohnić <matija.marohnic@gmail.com>
2022-01-09 00:31:34 +0100

    Set up basic MDX bundling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,29 @@
+import { useLoaderData } from 'remix'
+import type { LoaderFunction } from 'remix'
+import { bundleMDX } from '../mdx-bundler.server'
+
+interface LoaderData {
+  title: string
+}
+
+export const loader: LoaderFunction = async () => {
+  const result = await bundleMDX({
+    source: `
+---
+title: Hello world!
+---
+# Hello world!
+    `.trim()
+  })
+  return result.frontmatter
+}
+
 export default function Index() {
+  const meta = useLoaderData<LoaderData>()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <h2>Frontmatter title: {meta?.title ?? '...'}</h2>
       <ul>
         <li>
           <a
