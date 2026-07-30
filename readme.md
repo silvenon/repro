0f87eed0602f4f64b915cb98dea700ce3f99bbaf
Matija Marohnić <matija.marohnic@gmail.com>
2026-07-30 17:22:34 +0200

    Reproduce issue

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
-# Repro
+# Repro: Vitest v5.0.0-beta.7, projects and setup
 
-This is a collection of demos reproducing various bugs, separated into branches.
+I had been using Node v24 for this. What I've found out:
+
+- it only happens with `projects`, not if I collapse everything into top-level `test`
+- it only happens if `setup.ts` contains content, seemingly if it contains an import that augments vitest
+
+```sh
+pnpm install # install deps
+pnpm test:setup # install browsers
+pnpm test # run tests
+```
---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          name: "browser",
+          setupFiles: ["./setup.ts"],
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
+});
